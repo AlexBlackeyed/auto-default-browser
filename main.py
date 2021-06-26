f506cfac6bc960dbe5355afc260fe57442a80349
AlexBlackeyed <76808208+AlexBlackeyed@users.noreply.github.com>
2021-06-26 18:38:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,16 @@ import pyautogui
 from time import sleep
 
 pyautogui.press("win")
-sleep(.2)
-pyautogui.typewrite("default")
 sleep(.1)
+pyautogui.typewrite("defaul")
 pyautogui.press("enter")
-sleep(1.5)
-if pyautogui.locateCenterOnScreen('mozilla.png'):
-    x, y = pyautogui.locateCenterOnScreen('mozilla.png')
-    pyautogui.moveTo(x, y)
-    pyautogui.click(x, y)
-    sleep(.7)
-    x, y = pyautogui.locateCenterOnScreen('brave.png')
-    pyautogui.moveTo(x, y)
-    pyautogui.click(x, y)
-    pyautogui.hotkey('alt', 'f4')
-    print("Finished")
-    quit()
-else:
-    quit()
+sleep(1)
+pyautogui.press("enter")
+pyautogui.press("tab")
+pyautogui.press("enter")
+pyautogui.hotkey('alt', 'f4')
+quit()
+
+
 
 
